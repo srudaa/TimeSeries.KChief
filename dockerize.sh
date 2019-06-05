babf8d10d9ea56ab07d5410770f8dcda79d8493d
Einar Ingebrigtsen <einar@dolittle.com>
2019-06-05 19:38:34 +0200

    Adding no-cache

--- dockerize.sh
+++ dockerize.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export VERSION=$(git tag --sort=-version:refname | head -1)
-docker build -f ./Source/Dockerfile -t dolittle/timeseries-kchief:$VERSION .
+docker build --no-cache -f ./Source/Dockerfile -t dolittle/timeseries-kchief:$VERSION .
 docker push dolittle/timeseries-kchief:$VERSION
